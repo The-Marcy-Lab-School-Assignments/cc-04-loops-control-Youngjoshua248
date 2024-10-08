7ea09054c58739399b40b7f8d77797d3b3c40b93
Joshua Young <youngjoshua248@icloud.com>
2024-10-08 11:12:04 -0400

    CodeChallenge 4

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 //Write your solutions in this file, don't forget to test your functions.
+//const countFromOne = (nn) => {
+//for (let j = 1; j <= nn; j += 1) {
+//console.log(j);
+//}
+//};
+//countFromOne(50);
+
+//const countEveryOdd = (nn) => {
+//for (let j = 1; j <= nn; j++) {
+// if (j % 2 !== 0) {
+// console.log(j);
+//}
+//}
+//};
+//countEveryOdd(15);
+
+const isNegative = (j) => {
+  if (j < 0) {
+    return true;
+  } else {
+    return false;
+  }
+};
+isNegative(-3);
+console.log(isNegative(-3));
+
+const betweenFiveAndTwenty = (j) => {
+  if ((j) => 5 && j <= 20) {
+    return true;
+  } else {
+    return false;
+  }
+};
+console.log(betweenFiveAndTwenty(12));
+
+const sumOfThreeOrFive = (j) => {
+    if (j = 3 )
+};
