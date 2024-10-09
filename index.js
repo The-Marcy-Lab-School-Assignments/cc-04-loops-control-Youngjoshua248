9af04a3ef063030d2120e5b4024ff7caa8b8fb45
Joshua Young <youngjoshua248@icloud.com>
2024-10-09 10:23:44 -0400

    finished cc -04

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 //Write your solutions in this file, don't forget to test your functions.
-//const countFromOne = (nn) => {
-//for (let j = 1; j <= nn; j += 1) {
-//console.log(j);
-//}
-//};
-//countFromOne(50);
+const countFromOne = (nn) => {
+  for (let j = 1; j <= nn; j += 1) {
+    console.log(j);
+  }
+};
+countFromOne(50);
 
-//const countEveryOdd = (nn) => {
-//for (let j = 1; j <= nn; j++) {
-// if (j % 2 !== 0) {
-// console.log(j);
-//}
-//}
-//};
-//countEveryOdd(15);
+const countEveryOdd = (nn) => {
+  for (let j = 1; j <= nn; j++) {
+    if (j % 2 !== 0) {
+      console.log(j);
+    }
+  }
+};
+countEveryOdd(15);
 
 const isNegative = (j) => {
   if (j < 0) {
@@ -35,5 +35,22 @@ const betweenFiveAndTwenty = (j) => {
 console.log(betweenFiveAndTwenty(12));
 
 const sumOfThreeOrFive = (j) => {
-    if (j = 3 )
+  let sum = 0;
+  for (let j = 1; j < 1000; j++) {
+    if (j % 3 === 0 || j % 5 === 0) {
+      sum += j;
+      console.log(j);
+    }
+  }
+  return sum;
+};
+
+console.log(sumOfThreeOrFive());
+
+const isAllLowerCase = (str) => {
+  if (str.toLowerCase() === str) {
+    return true;
+  } else {
+    return false;
+  }
 };
